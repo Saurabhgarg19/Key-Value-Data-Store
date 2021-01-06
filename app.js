cd77b7aab238e324ccb03aa0997bbca4e87f07e5
Saurabh Garg <53430653+Saurabhgarg19@users.noreply.github.com>
2021-01-06 20:48:23 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,29 +1,11 @@
-const x = require("./readData");
-const create = require("./create");
+const Maindb = require('./maindb');
+const db = new Maindb();
 
-x("anubhav")
-  .then((data)=> {
-    console.log(data);
-  })
-  .catch((err)=> {
-    console.log(err);
-  })
 
-const data = JSON.stringify({
-  manik: {
-    name: "manik",
-    age: 34
-  },
-  rishabh:{
-    name: "rishabh",
-    age: 23
-  }
-});
-
-create(data, "mydata.json")
-  .then(()=> {
-    console.log('completed');
-  })
-  .catch((err)=> {
-    console.log(err)
-  })
+(async function(){
+    await db.create("users");
+    db.use("users","./datastore");
+    await db.insert("Saurabh",{age: 20});
+    console.log(await db.read("Saurabh"));
+    db.delete("Saurabh");
+})();
